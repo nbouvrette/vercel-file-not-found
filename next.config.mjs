82bf26fa6b50e93ae9c7fef364f74a6a23114799
Nicolas Bouvrette <bouvrette.nicolas@gmail.com>
2022-12-22 00:06:11 -0500

    build: try to get rid of the error by removing webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
-import { Config, webpackConfigurationHandler } from "next-multilingual/config";
-
-// Add extra Webpack configuration by wrapping next-multilingual's webpack handler.
-function webpack(config, context) {
-  // config = webpackConfigurationHandler(config, context);
-  config.module.rules.push(
-    {
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    },
-    {
-      test: /\.md$/,
-      use: "raw-loader",
-    }
-  );
-  return config;
-}
+import { Config } from "next-multilingual/config";
 
 const multilingualConfig = new Config(
   "exampleApp",
@@ -38,16 +22,6 @@ export const nextConfig = {
   },
   i18n,
   poweredByHeader: false,
-  webpack,
-  async rewrites() {
-    return multilingualConfig.getRewrites();
-  },
-  async redirects() {
-    return multilingualConfig.getRedirects();
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
