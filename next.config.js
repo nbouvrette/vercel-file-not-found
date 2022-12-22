b53bd5e6156930bee385573d09222c7477e890e4
Nicolas Bouvrette <bouvrette.nicolas@gmail.com>
2022-12-21 23:55:19 -0500

    build: try to reproduce the error by making more complex Next.js config

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const { getConfig } = require("next-multilingual/config");
-
-const config = getConfig("exampleApp", ["en-US", "fr-CA"], "en-US", {
-  poweredByHeader: false,
-});
-
-module.exports = config;
---
+++ next.config.mjs
@@ -0,0 +1,58 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+import { Config, webpackConfigurationHandler } from "next-multilingual/config";
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+// Add extra Webpack configuration by wrapping next-multilingual's webpack handler.
+function webpack(config, context) {
+  config = webpackConfigurationHandler(config, context);
+  config.module.rules.push(
+    {
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    },
+    {
+      test: /\.md$/,
+      use: "raw-loader",
+    }
+  );
+  return config;
+}
+
+const multilingualConfig = new Config(
+  "exampleApp",
+  ["en-US", "fr-CA"],
+  "en-US"
+);
+
+/** @type {import('next/dist/server/config-shared').I18NConfig} */
+export const i18n = {
+  locales: multilingualConfig.getUrlLocalePrefixes(),
+  defaultLocale: multilingualConfig.getDefaultUrlLocalePrefix(),
+  localeDetection: false,
+};
+
+/** @type {import('next').NextConfig} */
+export const nextConfig = {
+  outputFileTracing: false,
+  experimental: {
+    esmExternals: false,
+    scrollRestoration: true,
+  },
+  i18n,
+  poweredByHeader: false,
+  webpack,
+  async rewrites() {
+    return multilingualConfig.getRewrites();
+  },
+  async redirects() {
+    return multilingualConfig.getRedirects();
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default withBundleAnalyzer(nextConfig);
