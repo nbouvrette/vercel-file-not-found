55037a1b30bd9454b096696abb66324a30159a55
Nicolas Bouvrette <bouvrette.nicolas@gmail.com>
2022-12-21 20:16:13 -0500

    build: try to reproduce the error using a separate function

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,15 +2,20 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { readFileSync } from "node:fs";
 import path from "node:path";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const handler = async (
+  request: NextApiRequest,
+  response: NextApiResponse
+): Promise<void> => {
   console.log("info  - /api/hello has been called");
   try {
     const templateFile = path.join(process.cwd(), "emails", "default.mjml");
     const emailTemplate = readFileSync(templateFile, "utf8");
-    res.status(200).json({ emailTemplate });
+    response.status(200).json({ emailTemplate });
   } catch (error) {
     console.log("error - Error when calling hello.ts");
     console.dir(error);
-    res.status(500).end();
+    response.status(500).end();
   }
-}
+};
+
+export default handler;
