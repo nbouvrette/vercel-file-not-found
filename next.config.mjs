603caf6914c754f7143c5d0c9c1dd0c2850fe238
Nicolas Bouvrette <bouvrette.nicolas@gmail.com>
2022-12-22 00:24:40 -0500

    build: try to reproduce the error without outputFileTracing

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,28 @@
-import { Config } from "next-multilingual/config";
+import bundleAnalyzer from "@next/bundle-analyzer";
+import { Config, webpackConfigurationHandler } from "next-multilingual/config";
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+// Add extra Webpack configuration by wrapping next-multilingual's webpack handler.
+function webpack(config, context) {
+  config = webpackConfigurationHandler(config, context);
+  config.module.rules.push(
+    {
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    },
+    {
+      test: /\.md$/,
+      use: "raw-loader",
+    }
+  );
+  return config;
+}
 
 const multilingualConfig = new Config(
-  "exampleApp",
+  "avansaiApp",
   ["en-US", "fr-CA"],
   "en-US"
 );
@@ -15,12 +36,26 @@ export const i18n = {
 
 /** @type {import('next').NextConfig} */
 export const nextConfig = {
+  // outputFileTracing: false,
   experimental: {
     esmExternals: false,
     scrollRestoration: true,
   },
   i18n,
   poweredByHeader: false,
+  webpack,
+  async rewrites() {
+    return multilingualConfig.getRewrites();
+  },
+  async redirects() {
+    return multilingualConfig.getRedirects();
+  },
+  // @TODO remove!! temporary to test builds
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
